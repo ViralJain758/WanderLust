271340a04748780ff655d3eaae8189f57099f4a3
ViralJain758 <viraljain758@gmail.com>
2026-02-05 00:14:46 +0530

    Initialize Database

--- models/listing.js
+++ models/listing.js
@@ -2,19 +2,31 @@ const mongoose = require('mongoose');
 const schema = mongoose.Schema;
 
 const listingSchema = new schema({
-    title : {
-        type : String,
-        required : true,
+  title: {
+    type: String,
+    required: true,
+  },
+
+  description: String,
+
+  image: {
+    filename: {
+      type: String,
     },
-    description : String,
-    image : {
-        type : String,
-        default : "https://www.shutterstock.com/image-photo/sun-sets-behind-mountain-ranges-600nw-2479236003.jpg",
-        set : (v) => v === "" ? "https://www.shutterstock.com/image-photo/sun-sets-behind-mountain-ranges-600nw-2479236003.jpg" : v,
+    url: {
+      type: String,
+      default:
+        "https://www.shutterstock.com/image-photo/sun-sets-behind-mountain-ranges-600nw-2479236003.jpg",
+      set: (v) =>
+        v === ""
+          ? "https://www.shutterstock.com/image-photo/sun-sets-behind-mountain-ranges-600nw-2479236003.jpg"
+          : v,
     },
-    price : Number,
-    location : String,
-    country : String,
+  },
+
+  price: Number,
+  location: String,
+  country: String,
 });
 
 const Listing = mongoose.model("Listing", listingSchema);

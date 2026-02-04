43170772d4487c67d2376f9e131456dda5b81ac1
ViralJain758 <viraljain758@gmail.com>
2026-02-05 00:37:07 +0530

    Add Index Route

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const Listing = require("./models/listing.js")
 
@@ -17,25 +18,33 @@ async function main() {
     await mongoose.connect(MONGO_URL);
 }
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 
 app.get("/", (req, res) => {
     res.send("Hello World");
 });
 
-app.get("/testListings", async(req, res) => {
-    let sampleListing = new Listing({
-        title : "My New Villa",
-        description : "By the Beach",
-        image : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTaPgON7MfinJtGYnJxT-1LDC5PgQpEZmoRw&s",
-        price : 1200,
-        location : "Goa",
-        country : "India",
-    });
-
-    await sampleListing.save();
-    console.log("Sample Was Saved");
-    res.send("Successful Testing");
+app.get("/listings", async (req, res) => {
+    const listings = await Listing.find({});
+    res.render("listings/index.ejs", {listings})
 })
+
+// app.get("/testListings", async(req, res) => {
+//     let sampleListing = new Listing({
+//         title : "My New Villa",
+//         description : "By the Beach",
+//         image : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTaPgON7MfinJtGYnJxT-1LDC5PgQpEZmoRw&s",
+//         price : 1200,
+//         location : "Goa",
+//         country : "India",
+//     });
+
+//     await sampleListing.save();
+//     console.log("Sample Was Saved");
+//     res.send("Successful Testing");
+// })
 
 app.listen(8080, () =>{
     console.log("Server is started on http://localhost:8080/");

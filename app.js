94f8ada74d3aec81694fbd93c02f3b09ee237967
ViralJain758 <viraljain758@gmail.com>
2026-02-05 17:52:18 +0530

    Add Create Route

--- app.js
+++ app.js
@@ -2,42 +2,54 @@ const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
 
-const Listing = require("./models/listing.js")
+const Listing = require("./models/listing.js");
 
 const app = express();
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust"
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
-
-main().then(() => {
+main()
+  .then(() => {
     console.log("Connected");
-}).catch(err => {
+  })
+  .catch((err) => {
     console.log(err);
-})
+  });
 
 async function main() {
-    await mongoose.connect(MONGO_URL);
+  await mongoose.connect(MONGO_URL);
 }
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.use(express.urlencoded({extended: true}));
-
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.send("Hello World");
+  res.send("Hello World");
 });
 
 // Index Route
 app.get("/listings", async (req, res) => {
-    const listings = await Listing.find({});
-    res.render("listings/index.ejs", {listings})
+  const listings = await Listing.find({});
+  res.render("listings/index.ejs", { listings });
+});
+
+//New Route
+app.get("/listings/new", (req, res) => {
+  res.render("listings/new.ejs");
 });
 
 //Show Route
-app.get("/listings/:id", async(req, res) => {
-    let {id} = req.params;
-    const listing = await Listing.findById(id);
-    res.render("listings/show.ejs", {listing});
+app.get("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/show.ejs", { listing });
+});
+
+//Create Route
+app.post("/listings", async (req, res) => {
+  const newListing = new Listing(req.body.listing);
+  await newListing.save();
+  res.redirect("/listings");
 });
 
 // app.get("/testListings", async(req, res) => {
@@ -55,6 +67,6 @@ app.get("/listings/:id", async(req, res) => {
 //     res.send("Successful Testing");
 // })
 
-app.listen(8080, () =>{
-    console.log("Server is started on http://localhost:8080/");
+app.listen(8080, () => {
+  console.log("Server is started on http://localhost:8080/");
 });
